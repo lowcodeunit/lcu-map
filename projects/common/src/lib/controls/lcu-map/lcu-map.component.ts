5e9965f802a413a093eab4c57f53a4068310d335
powhound4 <george.hatch@fathym.com>
2019-07-24 14:46:59 -0600

    update from basic-info branch

--- projects/common/src/lib/controls/lcu-map/lcu-map.component.ts
+++ projects/common/src/lib/controls/lcu-map/lcu-map.component.ts
@@ -89,6 +89,8 @@ export class LcuMapComponent implements OnInit {
 
 
   // PROPERTIES 
+  //12 for bottom right & 9 for right bottom
+  public ZoomOptions: Object = { position: 9};
 
   /**
    * Indicates whether or not the layers dropdown will be displayed

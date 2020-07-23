2898706ad5aae6a2cbb54e23eefe589b44bd1560
William Horst <william.horst@fathym.com>
2020-07-23 12:20:45 -0600

    added ability to favorite / check activites

--- projects/common/src/lib/controls/map-journey/map-journey.component.ts
+++ projects/common/src/lib/controls/map-journey/map-journey.component.ts
@@ -40,6 +40,11 @@ export class MapJourneyComponent implements OnInit {
     this.normalizeAndEmitJourney('activity checked/unchecked', this.Journey);
   }
 
+  public onFavoritedClick(activity) {
+    activity.Favorited = !activity.Favorited;
+    this.normalizeAndEmitJourney('activity favorited / unfavorited', this.Journey);
+  }
+
   /**
    *
    * @param message the message to include as to what change was made to trigger the emit
@@ -48,10 +53,12 @@ export class MapJourneyComponent implements OnInit {
    * removes anything added to the journey that is specific to this map-journey component and emits a message and the changed journey
    */
   protected normalizeAndEmitJourney(message: string, journey: any) {
-    journey.ActivityGroups.forEach(ag => {
+    const journeyToSend = JSON.parse(JSON.stringify(journey));
+    journeyToSend.ActivityGroups.forEach(ag => {
       delete ag.PanelOpenState;
+      // DELETE ADDED PROPERTIES HERE
     });
-    this.JourneyChanged.emit({message, journey});
+    this.JourneyChanged.emit({message, journey: journeyToSend});
   }
 
 }

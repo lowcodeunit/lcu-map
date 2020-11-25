23e3697ad59cf2fda1943ae04d473b5e03b562cf
powhound4 <george.hatch@fathym.com>
2020-11-25 11:15:15 -0700

    remove console logs

--- projects/common/src/lib/controls/lcu-map/activity-location-window/activity-location-window.component.ts
+++ projects/common/src/lib/controls/lcu-map/activity-location-window/activity-location-window.component.ts
@@ -165,9 +165,7 @@ export class ActivityLocationWindowComponent implements OnInit, OnDestroy, OnCha
   public OnChoseIcon(icon: string) {
     this.IsSelectingIcon = false;
     this.UserChoseIcon.emit(icon);
-    // console.log("icon: ", this.marker.WidgetIcon)
     this.marker.WidgetIcon = icon;
-    // console.log("icon sstring: ", icon)
   }
 
   public ToggleFavorited(){
